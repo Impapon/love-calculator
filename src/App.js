bf46197cd43827bc681a68a5084d4404cc835f1a
real-mahadi <real.mahadihasan@gmail.com>
2021-09-19 16:28:33 +0600

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './css/App.css'
+const axios = require("axios").default;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [percentage, setPercentage] = useState(0);
+    let name = React.createRef()
+    let crushName = React.createRef()
+
+
+    function handleClick() {
+        let options = {
+            method: 'GET',
+            url: 'https://love-calculator.p.rapidapi.com/getPercentage',
+            params: { sname: 'mahadi', fname: 'hasan' }, 
+            headers: {
+                'x-rapidapi-host': 'love-calculator.p.rapidapi.com',
+                'x-rapidapi-key': 'ed794d5443msh73584a3c4a3274ap18cd2bjsn8819a8716de0'
+            }
+        };
+
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+
+
+    return (
+        <div className='container'>
+            <input ref={name} placeholder='Your name' />
+            <input ref={crushName} placeholder="Your Crush's  name" />
+            <button onClick={handleClick}>Submit</button>
+
+            <div className='show-result'>
+                <p>{percentage}</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
